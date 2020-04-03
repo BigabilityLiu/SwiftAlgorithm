7b6a5f7fef1d1a73c5c581fa52dee787af291107
刘旭东 <liuxudong@trendlab.ai>
2020-04-03 23:47:34 +0800

    Initial Commit

---
+++ SwiftAlgorithm/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftAlgorithm
+//
+//  Created by 刘旭东 on 2020/4/3.
+//  Copyright © 2020 LiuXudong. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
